71c3f5fcc834095ed46ae5d5a16baef7cd258998
Todd Nichols <toddnichols@Todds-iMac-2015.local>
2016-12-17 22:38:03 -0600

    add home module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from "./app.routes";
 import { AppComponent }  from './app.component';
 import { MainHeaderComponent } from "./main-header/main-header.component";
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
-    imports:      [ BrowserModule ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HomeModule
+    ],
     declarations: [
         AppComponent,
         MainHeaderComponent
     ],
-    bootstrap:    [ AppComponent ]
+    bootstrap: [ AppComponent ]
 })
 
 export class AppModule {
---
+++ app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    exports: [
+        RouterModule
+    ],
+    providers: [],
+})
+
+export class AppRoutingModule { }
